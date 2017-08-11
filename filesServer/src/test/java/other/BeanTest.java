58e70994d7fdc5b280c4cd0cf7ec180d955484bb
ming <348630367@qq.com>
2017-08-11 17:50:42 +0800

    delete files

--- filesServer/src/test/java/other/BeanTest.java
+++
@@ -1,9 +0,0 @@
-package other;
-
-import com.fasterxml.jackson.databind.util.BeanUtil;
-
-/**
- * Created by scream on 2017/7/27.
- */
-public class BeanTest {
-}
--- filesServer/src/test/java/util/TestConfiguration.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-/**
- * Created by lixiaodong on 2016/7/1.
- */
-public class TestConfiguration {
-    //public static final String HOST_IP = "172.24.63.14";
-    public static final String HOST_IP = "127.0.0.1";
-}
